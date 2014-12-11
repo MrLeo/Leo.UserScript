25c28839a2d4305ff4a04a5eb2df73e6e15bc8cc
MrLeo <lxbin6819@vip.qq.com>
2014-12-11 10:53:35 +0800

    修改test模板

--- test.user.js
+++ test.user.js
@@ -2,13 +2,13 @@
 // @name            MyUserScript
 // @namespace       http://use.i.E.your.homepage/
 // @description     userscript for mr.leo
+// @version         0.1
+// @updateURL       更新地址（UserScript.meta.js)
+// @downloadURL     更新地址（UserScript.user.js）
 // @match           http://*/*
 // @include         http://*/*
 // @exclude         http://*baidu.com/*
 // @require         http://code.jquery.com/jquery-latest.js
-// @updateURL       更新地址（UserScript.meta.js)
-// @downloadURL     更新地址（UserScript.user.js）
-// @version         0.1
 // ==/UserScript==
 
 //根据传入的URL，在head里生成script引用DOM对象
@@ -21,7 +21,7 @@ function createScriptLink(url) {
 }
 //在head引入JQuery
 ; (function () {
-    //window.jQuery || createScriptLink('//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js')
-    if (typeof jQuery == 'undefined') createScriptLink('//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js');
+    //if (typeof jQuery == 'undefined') createScriptLink('//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js');
+    window.jQuery || createScriptLink('//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js')
 })();
 //*******************************************************************************************************************//
