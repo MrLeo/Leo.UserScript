0dd4c07ed65b2457b7f6ad8f2a20f445f2730bf7
Leo <lxbin6819@vip.qq.com>
2016-06-24 00:16:31 +0800

    update

--- OneNote.user.js
+++ OneNote.user.js
@@ -13,13 +13,22 @@
 /**
  * OneNote 剪裁
  */
-var onenote = function () {
+function onenote() {
     var jsCode = document.createElement('script');
     jsCode.setAttribute('src', 'https://www.onenote.com/clipper/root?ClipperId=ON-6a191eef-84a2-4491-9cc2-2e4560cfb9af&ClipperType=Bookmarklet&ClipperVersion=2.0.2');
     jsCode.setAttribute('id', 'oneNoteCaptureRootScript');
     jsCode.setAttribute('type', 'text/javascript');
     document.body.appendChild(jsCode);
-};
+}
+
+/**
+ * 启动 OneNote 剪裁
+ * @constructor
+ */
+function OnenoteClipper() {
+    console.log("Clipper Hotkey Pressed")
+    onenote(); // Open OneNote Clipper.
+}
 
 /**
  * 监听键盘操作
@@ -32,15 +41,6 @@ var onenote = function () {
         }
     }, false);
 })();
-
-/**
- * 启动 OneNote 剪裁
- * @constructor
- */
-function OnenoteClipper() {
-    console.log("Clipper Hotkey Pressed")
-    onenote(); // Open OneNote Clipper.
-}
 
 //在“用户脚本命令”添加两个命令
 GM_registerMenuCommand('OneNote Clipper', OnenoteClipper);
