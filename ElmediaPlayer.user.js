77a858ecdf7c58630a26b4e747e46e39c2d78afe
xuebin.liu <xuebin.liu@zhaopin.com.cn>
2021-01-15 21:47:50 +0800

    feat: add ElmediaPlayer.user.js

---
+++ ElmediaPlayer.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name            Elmedia Player
+// @namespace       https://github.com/MrLeo/Leo.UserScript
+// @description     Elmedia Player (https://cdn.eltima.com/products/elmediaplayer/IntegrateIntoBrowser/)
+// @include         http://*
+// @include         https://*
+// @version         1.0
+// @require         https://cdn.bootcdn.net/ajax/libs/lodash.js/4.17.15/lodash.min.js
+// @updateURL       https://github.com/MrLeo/Leo.UserScript/raw/master/ElmediaPlayer.user.js
+// @downloadURL     https://github.com/MrLeo/Leo.UserScript/raw/master/ElmediaPlayer.user.js
+// @grant           GM_registerMenuCommand
+// ==/UserScript==
+
+function command() {
+  document.location.href='newelmediaplayer://'+encodeURIComponent(_.get(document.querySelector('#dpplayer video'),'src',location.href));
+}
+
+//在“用户脚本命令”添加两个命令
+GM_registerMenuCommand('Elmedia Player', command);
